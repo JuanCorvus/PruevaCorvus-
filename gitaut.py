e38ce0b38375b412b232f3e64ab21f7d1aad1d9a
JuanCorvus <nietzsche2.7182@gmail.com>
2021-11-12 23:35:14 -0600

    python2

--- gitaut.py
+++ gitaut.py
@@ -1,1 +1,20 @@
-from git import Repo 
+import os
+from git import Repo
+local_repo_directory = os.path.join(os.getcwd())
+
+def clone_repo():
+    Repo.clone_from("https://github.com/JuanCorvus/PruevaCorvus-.git",local_repo_directory)
+
+def chdirectory(path):
+    os.chdir(path)
+def update_file():
+    chdirectory(local_repo_directory)
+
+
+def main():
+    update_file()
+
+if __name__=="__main__":
+    main()
+
+
